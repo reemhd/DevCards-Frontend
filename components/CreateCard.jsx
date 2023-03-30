5c50e5c44486c1110c8b87c284b36185ad5e7e7e
danzo3000 <dbowman9213@gmail.com>
2023-03-30 10:44:55 +0100

    adds styling for createCard

--- components/CreateCard.jsx
+++ components/CreateCard.jsx
@@ -2,26 +2,28 @@ import React from "react";
 import { useState } from "react";
 import { Text, View, TextInput, StyleSheet } from "react-native";
 
-const CreateCard = ({ label }) => {
+const CreateCard = () => {
   const [cardFront, setCardFront] = useState("");
   const [cardBack, setCardBack] = useState("");
 
   return (
-    <View style={cardStyles.container}>
-      <Text style={label}>Front: </Text>
-      <TextInput
-        style={cardStyles.inputBox}
-        placeholder="Enter your question here"
-        value={cardFront}
-        onChangeText={(text) => setCardFront(text)}
-      />
-      <Text style={label}>Back: </Text>
-      <TextInput
-        style={cardStyles.inputBox}
-        placeholder="Enter your answer here"
-        value={cardBack}
-        onChangeText={(text) => setCardBack(text)}
-      />
+    <View style={cardStyles.outer}>
+      <View style={cardStyles.container}>
+        <Text style={cardStyles.label}>Front: </Text>
+        <TextInput
+          style={cardStyles.inputBox}
+          placeholder="Enter your question here"
+          value={cardFront}
+          onChangeText={(text) => setCardFront(text)}
+        />
+        <Text style={cardStyles.label}>Back: </Text>
+        <TextInput
+          style={cardStyles.inputBox}
+          placeholder="Enter your answer here"
+          value={cardBack}
+          onChangeText={(text) => setCardBack(text)}
+        />
+      </View>
     </View>
   );
 };
@@ -35,16 +37,38 @@ const cardStyles = StyleSheet.create({
     alignItems: "center",
     borderStyle: "solid",
     borderWidth: 1,
-    borderColor: "black",
+    borderColor: "#F9F9F9",
     margin: 50,
     padding: 20,
+    backgroundColor: "#27272D",
+    borderRadius: 8,
   },
   inputBox: {
     borderWidth: 1,
     borderStyle: "solid",
-    borderColor: "black",
+    borderColor: "#F9F9F9",
     padding: 10,
     margin: 10,
+    borderRadius: 8,
+    backgroundColor: "#818387",
+  },
+  label: {
+    fontWeight: "bold",
+    padding: 10,
+    elevation: 5,
+    backgroundColor: "#F99909",
+    color: "#F9F9F9",
+    alignItems: "center",
+    margin: 10,
+    width: "70%",
+    borderRadius: 10,
+    borderColor: "#F9F9F9",
+    borderWidth: 1,
+  },
+  outer: {
+    flex: 1,
+    backgroundColor: "#818387",
+    alignItems: "center",
   },
 });
 
