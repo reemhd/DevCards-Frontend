1387887b8ac3cb9cd001e1e088f9d3e86a79b12f
danzo3000 <dbowman9213@gmail.com>
2023-04-04 09:37:02 +0100

    adds message if deck is empty

--- components/SingleDeck.jsx
+++ components/SingleDeck.jsx
@@ -63,26 +63,42 @@ const SingleDeck = ({ route, navigation }) => {
   };
   return (
     <View style={singleDeckStyle.container}>
-      <Modal visible={isModalVisible} transparent={false}>
-        <View style={singleDeckStyle.container}>
-          <SingleCard cardID={cardID} deck={deck} />
-          <View style={singleDeckStyle.closeModal}>
-            <TouchableOpacity onPress={() => setIsModalVisible(false)}>
-              <Text style={singleDeckStyle.closeModalText}>Back to Deck</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      </Modal>
       <View style={singleDeckStyle.title}>
         <Text style={singleDeckStyle.titleText}>{title}</Text>
       </View>
-      <FlatList
-        data={deck}
-        renderItem={({ item }) => (
-          <Card front={item.front} back={item.back} _id={item._id} />
+      <>
+        {deck.length === 0 ? (
+          <View style={singleDeckStyle.empty}>
+            <Text style={singleDeckStyle.emptyText}>
+              This deck is currently empty. Click below to add a card!
+            </Text>
+          </View>
+        ) : (
+          <>
+            <Modal visible={isModalVisible} transparent={false}>
+              <View style={singleDeckStyle.container}>
+                <SingleCard cardID={cardID} deck={deck} />
+                <View style={singleDeckStyle.closeModal}>
+                  <TouchableOpacity onPress={() => setIsModalVisible(false)}>
+                    <Text style={singleDeckStyle.closeModalText}>
+                      Back to Deck
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </Modal>
+
+            <FlatList
+              data={deck}
+              renderItem={({ item }) => (
+                <Card front={item.front} back={item.back} _id={item._id} />
+              )}
+              keyExtractor={(item) => item._id}
+            />
+          </>
         )}
-        keyExtractor={(item) => item._id}
-      />
+      </>
+
       <Pressable
         style={singleDeckStyle.button}
         title="Create a New Card"
@@ -106,6 +122,18 @@ const singleDeckStyle = StyleSheet.create({
     fontSize: 20,
     padding: 20,
     color: "black",
+  },
+  empty: {
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1,
+    backgroundColor: "#27272D",
+  },
+  emptyText: {
+    color: "white",
+    fontSize: 22,
+    fontWeight: "bold",
+    margin: 20,
   },
   closeModal: {
     backgroundColor: "#F99909",
